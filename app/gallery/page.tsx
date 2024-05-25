fba90292009ed7f3ff3c13927a68b8d8ee19a5dd
Bertrand Bourion <bertrandbou@hotmail.fr>
2024-05-25 14:20:41 +0200

    style: add border radius to gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -93,7 +93,7 @@ const Gallery = () => {
         alt={alt}
         style={{
           ...style,
-          borderRadius: '0.2vw',
+          borderRadius: '1vw',
           boxShadow:
             '0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)',
           backgroundColor: 'white',
